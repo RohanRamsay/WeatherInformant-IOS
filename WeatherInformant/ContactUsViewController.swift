e95105fa1c5bf419c7d712b4fa863517524f7b53
Rohan <ramsay.rohan@gmail.com>
2017-01-21 10:45:45 +1300

    informational pages added.

--- WeatherInformant/ContactUsViewController.swift
+++ WeatherInformant/ContactUsViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ContactUsViewController: UIViewController {
 
+    var number = "+640211147224"
+    var mail = "ramsayroha@vuw.ac.nz"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,14 +28,22 @@ class ContactUsViewController: UIViewController {
         
         self.navigationController?.dismiss(animated: true, completion: nil)
     }
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func callButtonPressed(_ sender: Any) {
+        guard let number = URL(string: "telprompt://" + number) else { return }
+        UIApplication.shared.open(number, options: [:], completionHandler: nil)
     }
-    */
+    
+
+    @IBAction func textButtonPressed(_ sender: Any) {
+        
+        guard let number = URL(string: "sms://" + number) else { return }
+        UIApplication.shared.open(number, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func emailButtonPressed(_ sender: Any) {
+        guard let email = URL(string: "mailto://" + mail) else { return }
+        UIApplication.shared.open(email, options: ["subject":"Regarding Weather Informant"], completionHandler: nil)
+    }
 
 }
--- WeatherInformant/PrivacyPolicyViewController.swift
+++ WeatherInformant/PrivacyPolicyViewController.swift
@@ -10,10 +10,23 @@ import UIKit
 
 class PrivacyPolicyViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.textView?.text = " We firmly believe that privacy [is] both inconsequential and unimportant to you. If it were not, you probably would not have a Facebook, Twitter, or LinkedIn account: and you certainly wouldn't ever use a search engine like Google. If you're one of those tin-foil-hat wearing crazies that actually cares about privacy: stop using our services and get a life. \n\n We agree with Mark Zuckerberg when he pithily opined \"The age of Privacy is Over.\"" +
+        
+        "\n\n Occasionally, we make our subscriber mailing list available to third parties for marketing purposes and to carefully screened companies offering products and services that we believe would interest our readers. If you do not want to receive these offers, please email your name and address to: info@funnytimes.com and request that we not share your name and address with third parties." + "\n\n" +
+        
+        "The information that you give us and information about your order may be combined with other personally identifiable information (such as demographic information and past purchase history) available from our records and other sources. This information will be used to make our future marketing efforts more efficient. \n\n"
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        self.textView.scrollRangeToVisible(NSRange(location: 0, length: 1))
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,15 +37,5 @@ class PrivacyPolicyViewController: UIViewController {
         
         self.navigationController?.dismiss(animated: true, completion: nil)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
